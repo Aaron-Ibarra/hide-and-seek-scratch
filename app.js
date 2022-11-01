aabc05dc009947fa5a1b5087a73ca899505ae828
Aaron Ibarra <aaronibarra132@gmail.com>
2022-11-01 09:57:09 -0700

    js consts state and button events and additional html adjustments

--- app.js
+++ app.js
@@ -1,9 +1,39 @@
 /* Get DOM Elements */
-const firstButton = document.querySelector;
+const firstButton = document.getElementById('btn-1');
+const secondButton = document.getElementById('btn-2');
+const thirdButton = document.getElementById('btn-3');
+
+const firstGhost = document.getElementById('ghost-1');
+const secondGhost = document.getElementById('ghost-2');
+const thirdGhost = document.getElementById('ghost-3');
 
 /* State */
+const houses = ['house-1', 'house-2', 'house-3'];
+
+let totalGuesses = 0;
+let correctGuesses = 0;
 
 /* Events */
+firstButton.addEventListener('click', () => {
+    const hauntedHouse = Math.floor(Math.random() * 3);
+    const correctAnswer = houses[hauntedHouse];
+    console.log(correctAnswer);
+    // calculateGuess(correctAnswer, 'house-1');
+});
+
+secondButton.addEventListener('click', () => {
+    const hauntedHouse = Math.floor(Math.random() * 3);
+    const correctAnswer = houses[hauntedHouse];
+    console.log(correctAnswer);
+    // calculateGuess(correctAnswer, 'house-2');
+});
+
+thirdButton.addEventListener('click', () => {
+    const hauntedHouse = Math.floor(Math.random() * 3);
+    const correctAnswer = houses[hauntedHouse];
+    console.log(correctAnswer);
+    // calculateGuess(correctAnswer, 'house-3');
+});
 
 /* Display Functions */
 
